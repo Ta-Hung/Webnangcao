6181491a741231201b4fe19a5ca9a6e0ebb4be18
Ta-Hung <hungxtop@gmail.com>
2023-11-20 22:58:48 +0700

    update 2.0

--- MaverikStudio/MaverikStudio/assets/plugins/ckeditor/config.js
+++ MaverikStudio/MaverikStudio/assets/plugins/ckeditor/config.js
@@ -3,8 +3,16 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    config.filebrowserBrowseUrl = '/assets/plugins/ckfinder/ckfinder.html';
+    config.filebrowserImageBrowseUrl  = '/assets/plugins/ckfinder/ckfinder.html?type=Images';
+    config.filebrowserFlashBrowseUrl  = '/assets/plugins/ckfinder/ckfinder.html?type=Flash';
+    config.filebrowserUploadUrl  = '/assets/plugins/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files';
+    config.filebrowserImageUploadUrl  = '/assets/plugins/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Images';
+    config.filebrowserFlashUploadUrl  = '/assets/plugins/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';
+    config.filebrowserWindowWidth  = '1000';
+    config.filebrowserWindowHeight = '700';
 };
